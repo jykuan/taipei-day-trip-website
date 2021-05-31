cae27500ef1b6506d658ba6ed3c02e09a67a277a
Jamie Kuan <jamie871225@gmail.com>
2021-05-31 17:49:46 +0800

    Week05

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.before_request
 def before_request():
-    if request.path in ["", "/", "/api/attraction/<attractionId>", "/api/attractions", "/api/user", "/attraction/<id>"] or "templates" in request.path:
+    if request.path in ["", "/", "/api/attraction/<attractionId>", "/api/attractions", "/api/user", "/attraction/<id>", "/booking"] or "templates" in request.path:
         return None
     session.permanent=True
     user=session.get("status")
@@ -156,16 +156,36 @@ def apiAttractions():
 def apiBooking():
 	replyMessage={}
 	if request.method == "POST":
-		global tripScheduled
-		tripScheduled=[]
-		inquireAttractionId=request.get_json()
-		print(session["status"])
-		print(inquireAttractionId)
-		if inquireAttractionId["attractionId"] != "" and inquireAttractionId["date"] != "" and inquireAttractionId["price"] != "":
-			tripScheduled.append(inquireAttractionId)
+		inquireAttraction=request.get_json()
+		if inquireAttraction["attraction"] != "" and inquireAttraction["date"] != "" and inquireAttraction["price"] != "":
+			sqlSearchResult={}
+			cursorTripData.execute("SELECT _id, stitle, address, images FROM TaipeiTripData where _id=%s;", (inquireAttraction["attraction"],))
+			for (_id, stitle, address, images) in cursorTripData: 
+				sqlSearchResult={
+					"id":_id,
+					"name":stitle,
+					"address":address,
+					"image":images
+				}
+			pdimage=[]
+			spImages=sqlSearchResult["image"].split('"')
+			for spImage in spImages:
+				img=str(spImage)
+				if len(img) > 3:
+					if img[-3]+img[-2]+img[-1] == "jpg":
+						pdimage.append(img)
+					if img[-3]+img[-2]+img[-1] == "JPG":
+						pdimage.append(img)
+					if img[-3]+img[-2]+img[-1] == "png":
+						pdimage.append(img)
+					if img[-3]+img[-2]+img[-1] == "PNG":
+						pdimage.append(img)
+			sqlSearchResult["image"]=pdimage[0]
+			inquireAttraction["attraction"]=sqlSearchResult
+			session["spotScheduled"].append(inquireAttraction)
 			replyMessage["ok"]=True
 			return jsonify(replyMessage), 200
-		elif inquireAttractionId["attractionId"] == "" or inquireAttractionId["date"] == "":
+		elif inquireAttraction["attraction"] == "" or inquireAttraction["date"] == "":
 			replyMessage["error"]=True
 			replyMessage["message"]="格式錯誤或未填寫完成"
 			print("Sorry")
@@ -175,11 +195,12 @@ def apiBooking():
 			replyMessage["message"]="未登入系統"
 			return jsonify(replyMessage), 403
 	if request.method == "GET":
-		print(session["status"])
-		print(tripScheduled)
-		return jsonify(tripScheduled), 200
+		username=session.get("username")
+		print(session["spotScheduled"])
+		return jsonify(session["spotScheduled"]), 200
 	if request.method == "DELETE":
 		replyMessage["ok"]=True
+		session["spotScheduled"]=[]
 		return jsonify(replyMessage), 200
 
 @app.route("/api/user", methods=["GET", "POST", "PATCH", "DELETE"])
@@ -187,15 +208,16 @@ def apiUser():
 	sqlSearchResult={}
 	replyMessage={}
 	if request.method == "GET":
-		cursorTripData.execute("select id, email, password from user where email=%s;", (session.get("status"),))
-		for (id, email, password) in cursorTripData:
+		cursorTripData.execute("select name, email, password from user where email=%s;", (session.get("status"),))
+		for (name, email, password) in cursorTripData:
 			sqlSearchResult={
 				"data":{
-					"id":id,
+					"name":name,
 					"email":email,
 					"password":password
 				}
 			}
+		print(sqlSearchResult)
 		userStatus=session.get("status")
 		if userStatus == False:
 			sqlSearchResult={}
@@ -226,15 +248,18 @@ def apiUser():
 			return jsonify(replyMessage), 400
 	if request.method == "PATCH":
 		inquireUserData=request.get_json()
-		cursorTripData.execute("select email, password from user where email=%s;", (inquireUserData["signInEmail"],))
-		for (email, password) in cursorTripData:
+		cursorTripData.execute("select name, email, password from user where email=%s;", (inquireUserData["signInEmail"],))
+		for (name, email, password) in cursorTripData:
 			sqlSearchResult={
+				"name":name,
 				"email":email,
 				"password":password
 			}
 		if cursorTripData.rowcount>0:
 			if sqlSearchResult["email"]==inquireUserData["signInEmail"] and sqlSearchResult["password"]==inquireUserData["signInPassword"]:
+				session["username"]=sqlSearchResult["name"]
 				session["status"]=sqlSearchResult["email"]
+				session["spotScheduled"]=[]
 				replyMessage["ok"]=True
 				return jsonify(replyMessage), 200
 			else:
@@ -248,6 +273,7 @@ def apiUser():
 	if request.method == "DELETE":
 		print(str(session["status"])+", Logged Out")
 		session["status"]=False
+		session["spotScheduled"]=False
 		replyMessage["ok"]=True
 		return jsonify(replyMessage), 200
 
@@ -257,22 +283,18 @@ def attraction(id):
 
 @app.route("/booking")
 def booking():
-	# userStatus=session.get("status")
-	# print(userStatus)
-	# if userStatus == False:
-	# 	return render_template("index.html")
 	return render_template("booking.html")
 
 @app.route("/thankyou")
 def thankyou():
 	return render_template("thankyou.html")
 	
-# @app.errorhandler(500)
-# def internal_error(error):
-# 	sqlSearchResult={
-# 		"error":True,
-# 		"message":"Error"
-# 	}
-# 	return json.dumps(sqlSearchResult), 500
+@app.errorhandler(500)
+def internal_error(error):
+	sqlSearchResult={
+		"error":True,
+		"message":"Error"
+	}
+	return json.dumps(sqlSearchResult), 500
 
 app.run(host="0.0.0.0", port=3000, debug=True)
